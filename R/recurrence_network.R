3ed711f47752f505bbba342bd91e8af0a86be77e
Bartlomiej Klimowski <bklimowski1@gmail.com>
2019-04-13 21:59:09 +0200

    recurrence network

--- R/recurrence_network.R
+++ R/recurrence_network.R
@@ -1,9 +1,32 @@
-
-
-r <- cumsum(rnorm(1000))
-res <- matrix(0, 1000, 1000)
-eps<- 0.2
-for (ii in 1:length(r)) {
-  res[ii,] <- abs(r - r[ii]) < eps
+#' @title recurrence_network
+#' @description Create recurrence network
+#' @param time_series.
+#' @return Recurrence network as igraph object.
+#' @examples
+#' recurrence_network(rnorm(100), 0.5)
+#' @export
+recurrence_network <- function(ts, eps) {
+  ts %>%
+    recurrence_matrix(eps) %>%
+    igraph::graph_from_adjacency_matrix() %>%
+    return()
 }
-image(res, col=gray.colors(2, start = 1, end = 0))
+
+#' @title Recurrence matrix
+#' @description Create recurrence matrix
+#' @param time_series.
+#' @return Recurrence matrix
+#' @examples
+#' recurrence_matrix(rnorm(100), 0.5)
+#' @export
+recurrence_matrix <- function(ts, eps) {
+  recurrence_mat <- matrix(0, length(ts), length(ts))
+
+  for (ii in 1:length(ts)) {
+    recurrence_mat[ii,] <- abs(ts - ts[ii]) < eps
+  }
+
+  return(recurrence_mat)
+}
+
+
